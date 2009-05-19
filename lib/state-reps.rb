911489cea3b297336b2ca7788dce55d96498e047
Daniel Jackoway <danjdel@gmail.com>
2009-05-18 22:28:12 -0400

    move finding zip 9 to a function to find for just 1. what was I thinking when I wrote this?

--- lib/state-reps.rb
+++ lib/state-reps.rb
@@ -57,6 +57,30 @@ require 'uri'
   end
 
 
+  def zip_9(person)
+    begin    
+      url = URI.parse("http://zip4.usps.com/zip4/zcl_0_results.jsp?visited=1&pagenumber=0&firmname=&address2=#{spacesToPlus(person.address).upcase}&address1=&city=#{spacesToPlus(person.city).upcase}&state=#{spacesToPlus(person.state).upcase}&urbanization=&zip5=&submit.x=0&submit.y=0&submit=Find+ZIP+Code")
+      #puts url
+      response = Net::HTTP.get(url)
+      
+      page = response.to_s
+      page = page[31400..-1]
+      #puts page
+      #regex = /#{spacesToHTML(person.city)}&nbsp;#{spacesToHTML(person.state)}&nbsp;\d\d\d\d\d-\d\d\d\d/i
+      regex = /\d\d\d\d\d-\d\d\d\d/
+      #regex = /Newark&nbsp;DE&nbsp;&nbsp;19711-2334/i
+      #regex = /<br \/>/
+      #puts regex
+      #zipspot = (/#{spacesToHTML(person.address).upcase}#{spacesToHTML(person.city)}&nbsp;#{spacesToHTML(x.state)}&nbsp;\d\d\d\d\d-\d\d\d\d/ =~ page)
+      zipspot = regex=~page
+      zip = page[zipspot,10] if (zipspot)
+      #puts zip
+      zip
+    rescue
+      puts $!
+      puts x.name
+    end
+  end
 
   def getZip9()
 
@@ -89,29 +113,8 @@ require 'uri'
       #uri2 = "/zip4/zcl_0_results.jsp?visited=1&pagenumber=0&firmname=&address2=#{spacesToPlus(x.address).upcase}&address1=&city=#{spacesToPlus(x.city).upcase}&state=#{spacesToPlus(x.state).upcase}&urbanization=&zip5=&submit.x=0&submit.y=0&submit=Find+ZIP+Code"
       #uri2 = "/popular/24hours"
       
-      begin    
-        url = URI.parse("http://zip4.usps.com/zip4/zcl_0_results.jsp?visited=1&pagenumber=0&firmname=&address2=#{spacesToPlus(x.address).upcase}&address1=&city=#{spacesToPlus(x.city).upcase}&state=#{spacesToPlus(x.state).upcase}&urbanization=&zip5=&submit.x=0&submit.y=0&submit=Find+ZIP+Code")
-        #puts url
-        response = Net::HTTP.get(url)
-        
-        page = response.to_s
-        page = page[31400..-1]
-        #puts page
-        #regex = /#{spacesToHTML(x.city)}&nbsp;#{spacesToHTML(x.state)}&nbsp;\d\d\d\d\d-\d\d\d\d/i
-        regex = /\d\d\d\d\d-\d\d\d\d/
-        #regex = /Newark&nbsp;DE&nbsp;&nbsp;19711-2334/i
-        #regex = /<br \/>/
-        #puts regex
-        #zipspot = (/#{spacesToHTML(x.address).upcase}#{spacesToHTML(x.city)}&nbsp;#{spacesToHTML(x.state)}&nbsp;\d\d\d\d\d-\d\d\d\d/ =~ page)
-        zipspot = regex=~page
-        zip = page[zipspot,10] if (zipspot)
-        #puts zip
-        x.zip9 = zip if zip
-      rescue
-        puts $!
-        puts x.name
-      end
-      
+      zip = zip_9 x
+      x.zip9 = zip if zip
       
       #thanks to http://snakesgemscoffee.blogspot.com/2007/03/twittering-around.html
     }
