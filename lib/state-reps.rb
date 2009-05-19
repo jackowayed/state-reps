a454d1317aaa98b12941b2835cfc0395346f1b97
Daniel Jackoway <danjdel@gmail.com>
2009-05-18 23:16:35 -0400

    fix typo. fix index of where to start looking for zip. change an x to a person

--- lib/state-reps.rb
+++ lib/state-reps.rb
@@ -5,7 +5,7 @@ require 'person'
 require 'net/http'
 require 'uri'
   def spacesToPlus(str)
-    str = str.to_sq
+    str = str.to_s
     arr = str.split(" ")
     str = ""
     arr.each{|x|
@@ -58,13 +58,13 @@ require 'uri'
 
 
   def zip_9(person)
-    begin    
+    begin
       url = URI.parse("http://zip4.usps.com/zip4/zcl_0_results.jsp?visited=1&pagenumber=0&firmname=&address2=#{spacesToPlus(person.address).upcase}&address1=&city=#{spacesToPlus(person.city).upcase}&state=#{spacesToPlus(person.state).upcase}&urbanization=&zip5=&submit.x=0&submit.y=0&submit=Find+ZIP+Code")
       #puts url
       response = Net::HTTP.get(url)
       
       page = response.to_s
-      page = page[31400..-1]
+      page = page[32000..-1]
       #puts page
       #regex = /#{spacesToHTML(person.city)}&nbsp;#{spacesToHTML(person.state)}&nbsp;\d\d\d\d\d-\d\d\d\d/i
       regex = /\d\d\d\d\d-\d\d\d\d/
@@ -78,7 +78,7 @@ require 'uri'
       zip
     rescue
       puts $!
-      puts x.name
+      puts person.name
     end
   end
 
